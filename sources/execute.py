1ee0c537ae6f83a352473b0f0e9d755b1b8eaea3
Zern <75311133+BG47510@users.noreply.github.com>
2023-09-03 12:28:42 +0200

    Update execute.py

--- sources/execute.py
+++ sources/execute.py
@@ -1,25 +1,25 @@
 import os
 import sys
 #import lci
-#import frinfo
+import frinfo
 #import fr02
 #import fr03
-import fr04
+#import fr04
 
 
 #file_lci = "lci.py"
-#file_frinfo = "frinfo.py"
+file_frinfo = "frinfo.py"
 #file_fr02 = "fr02.py"
 #file_fr03 = "fr03.py"
-file_fr04 = "fr04.py"
+#file_fr04 = "fr04.py"
 
 
 content = """
 #open(file_lci).read()
-#open(file_frinfo).read()
+open(file_frinfo).read()
 #open(file_fr02).read()
 #open(file_fr03).read()
-open(file_fr04).read()
+#open(file_fr04).read()
 
 
 """
