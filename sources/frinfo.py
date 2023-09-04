205dc6f78922d6e12e0c8a56597a9053d64ba2a2
Zern <75311133+BG47510@users.noreply.github.com>
2023-09-04 14:50:01 +0200

    Update frinfo.py

--- sources/frinfo.py
+++ sources/frinfo.py
@@ -2,7 +2,7 @@
 
 import requests
 import os
-import sys
+#import sys
 
 
 headers = {'User-Agent': 'QwantMobile/2.0 (Android 5.1; Tablet; rv:61.0) Gecko/61.0 Firefox/59.0 QwantBrowser/61.0'}
@@ -23,7 +23,7 @@ source = response.text
 lien = source.replace("index", "France_Info-avc1_2500000=10001")
 
 lien2 = source.replace("index", "France_Info-mp4a_96000_fra=20000")
-#print(f'#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio-AACL-96",LANGUAGE="fr",NAME="Francais",DEFAULT=YES,AUTOSELECT=YES,CHANNELS="2",URI="{lien2}"')
+
 
 
 # Les f-string permettent d’insérer des variables ou expressions à l'intérieur d'une chaine de caractères.
@@ -42,7 +42,6 @@ flux = (f'\n#EXTM3U\n#EXT-X-VERSION:5\n#EXT-X-STREAM-INF:BANDWIDTH=3032655,'
 flux2 = (f'\n#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio-AACL-96", \
 LANGUAGE="fr",NAME="Francais",DEFAULT=YES,AUTOSELECT=YES, \
 CHANNELS="2",URI="{lien2}"')
-print(flux2)
 
 
 m3u8 = (flux + flux2)
