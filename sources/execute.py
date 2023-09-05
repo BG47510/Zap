f56d92daf1c5656dff621429c839d66cccf475e9
Zern <75311133+BG47510@users.noreply.github.com>
2023-09-05 20:26:53 +0200

    Update execute.py

--- sources/execute.py
+++ sources/execute.py
@@ -1,14 +1,15 @@
 #import os
 #import sys
 #import lci
-#import frinfo
+import frinfo
 #import fr02
 #import fr03
 #import fr04
 
 
 #file_lci = "lci.py"
-file_frinfo = "sh ${GITHUB_WORKSPACE}sources/frinfo.py"
+#file_frinfo = "sh ${GITHUB_WORKSPACE}sources/frinfo.py"
+file_frinfo = "frinfo.py"
 #file_fr02 = "fr02.py"
 #file_fr03 = "fr03.py"
 #file_fr04 = "fr04.py"
