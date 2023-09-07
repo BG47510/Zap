88a3f4cfbfdb4b7855772ddbdf0fcc6fdca83223
Zern <75311133+BG47510@users.noreply.github.com>
2023-09-07 14:57:06 +0200

    Update execute.py

--- sources/execute.py
+++ sources/execute.py
@@ -9,17 +9,17 @@
 
 #file_lci = "lci.py"
 #file_frinfo = "sh ${GITHUB_WORKSPACE}sources/frinfo.py"
-#file_frinfo = "frinfo.py"
+file_frinfo = "frinfo.py"
 #file_fr02 = "fr02.py"
 #file_fr03 = "fr03.py"
 #file_fr04 = "fr04.py"
 
 
-#content = """
+content = """
 #open(file_lci).read()
-#open(file_frinfo).read()
+open(file_frinfo).read()
 #open(file_fr02).read()
 #open(file_fr03).read()
 #open(file_fr04).read()
-#"""
-#exec(content)
+"""
+exec(content)
