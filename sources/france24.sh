b04d428de36fc700af7257a84cd3877501127eee
Zern <75311133+BG47510@users.noreply.github.com>
2023-09-13 15:50:55 +0200

    Update france24.sh

--- sources/france24.sh
+++ sources/france24.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-echo $(dirname $0)
 
 python3 -m pip install requests
 
-cd $(dirname $0)/sources/
 
-python3 france24.py > ./france24.m3u8
+python3 sources/france24.py > direct/france24.m3u8
 
-echo m3u grabbed
+exit
