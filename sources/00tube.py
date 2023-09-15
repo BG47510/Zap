24556cc0d39ef7dedca43f4393a73899fbe1bf73
Zern <75311133+BG47510@users.noreply.github.com>
2023-09-15 16:06:47 +0200

    Update 00tube.py

--- sources/00tube.py
+++ sources/00tube.py
@@ -2,7 +2,7 @@ import cv2
 import vidgear
 
 
-stream = CamGear(source="https://www.youtube.com/watch?v=VIk_6OuYkSo", y_tube =True, time_delay=1, logging=True).start() # YouTube Video URL as input
+stream = CamGear(source="https://www.youtube.com/watch?v=gxG3pdKvlIs", y_tube =True, time_delay=1, logging=True).start() # YouTube Video URL as input
 
 while True:
     frame = stream.read()
