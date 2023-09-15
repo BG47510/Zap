6aae2c0a976661a9ce219b004ebbf44541f77b55
Zern <75311133+BG47510@users.noreply.github.com>
2023-09-15 16:37:46 +0200

    Update 00tube.py

--- sources/00tube.py
+++ sources/00tube.py
@@ -2,18 +2,40 @@ import cv2
 import vidgear
 
 # URL de la vidéo YouTube en entrée
-stream = CamGear(source="https://www.youtube.com/watch?v=gxG3pdKvlIs", y_tube =True, time_delay=1, logging=True).start() # YouTube Video URL as input
-print(stream)
+# import required libraries
+
+#from vidgear.gears import CamGear
+
+# Add YouTube Video URL as input source (for e.g https://youtu.be/uCy5OuSQnyA)
+# and enable Stream Mode (`stream_mode = True`)
+stream = CamGear(
+    source="https://youtu.be/uCy5OuSQnyA", 
+    stream_mode=True,
+    logging=True
+).start()
+
+# loop over
 while True:
+
+    # read frames from stream
     frame = stream.read()
+
+    # check for frame if Nonetype
     if frame is None:
-      break
-      
-    cv2.imshow("Output Frame", frame)
-    key = cv2.waitKey(30)
+        break
 
+    # {do something with the frame here}
+
+    # Show output window
+    cv2.imshow("Output", frame)
+
+    # check for 'q' key if pressed
+    key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
-      break
+        break
 
+# close output window
 cv2.destroyAllWindows()
+
+# safely close video stream
 stream.stop()
