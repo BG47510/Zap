f5ab9a836cf1bcb52d762e32aada8022d37299f3
Zern <75311133+BG47510@users.noreply.github.com>
2023-09-15 16:04:16 +0200

    Update 00tube.py

--- sources/00tube.py
+++ sources/00tube.py
@@ -5,15 +5,15 @@ import vidgear
 stream = CamGear(source="https://www.youtube.com/watch?v=VIk_6OuYkSo", y_tube =True, time_delay=1, logging=True).start() # YouTube Video URL as input
 
 while True:
-... frame = stream.read()
-... if frame is None:
-....... break
+    frame = stream.read()
+    if frame is None:
+      break
+      
+    cv2.imshow("Output Frame", frame)
+    key = cv2.waitKey(30)
 
-... cv2.imshow("Output Frame", frame)
-... key = cv2.waitKey(30)
-
-... if key == ord("q"):
-....... break
+    if key == ord("q"):
+      break
 
 cv2.destroyAllWindows()
 stream.stop()
