95c3ab41b9b82d97af2d3103c35eddc9717f1873
Zern <75311133+BG47510@users.noreply.github.com>
2023-09-15 16:08:49 +0200

    Update 00tube.py

--- sources/00tube.py
+++ sources/00tube.py
@@ -3,7 +3,7 @@ import vidgear
 
 # URL de la vidéo YouTube en entrée
 stream = CamGear(source="https://www.youtube.com/watch?v=gxG3pdKvlIs", y_tube =True, time_delay=1, logging=True).start() # YouTube Video URL as input
-
+print(stream)
 while True:
     frame = stream.read()
     if frame is None:
