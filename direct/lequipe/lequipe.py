3213bcc52ca6b974cfe391ad0f395e249bcac6bf
Zern <75311133+BG47510@users.noreply.github.com>
2023-09-20 16:59:43 +0200

    Update lequipe.py

--- direct/lequipe/lequipe.py
+++ direct/lequipe/lequipe.py
@@ -15,19 +15,7 @@ print("#EXTM3U")
 print("#EXT-X-VERSION:3")
 print("#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000")
 s = requests.Session()
-with open("direct/lequipe/lequipe.txt") as f:
-    for line in f:
-        line = line.strip()
-        if not line or line.startswith("~~"):
-            continue
-        if not line.startswith("https:"):
-            line = line.split("|")
-            ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
-            tvg_logo = line[2].strip()
-            tvg_id = line[3].strip()
-        else:
-            grab(line)
+
 
 
 def grab(line):
@@ -54,7 +42,19 @@ def grab(line):
     finally:
         print(m3u8)
 
-
+with open("direct/lequipe/lequipe.txt") as f:
+    for line in f:
+        line = line.strip()
+        if not line or line.startswith("~~"):
+            continue
+        if not line.startswith("https:"):
+            line = line.split("|")
+            ch_name = line[0].strip()
+            grp_title = line[1].strip().title()
+            tvg_logo = line[2].strip()
+            tvg_id = line[3].strip()
+        else:
+            grab(line)
 
 
 if "temp.txt" in os.listdir():
