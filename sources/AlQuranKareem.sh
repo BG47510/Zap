e45212c6565cc060d22da4b5a476f8c80136b910
Zern <75311133+BG47510@users.noreply.github.com>
2023-09-20 15:08:17 +0200

    Update AlQuranKareem.sh

--- sources/AlQuranKareem.sh
+++ sources/AlQuranKareem.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo $(dirname $0)
+#echo $(dirname $0)
 
-python3 -m pip install requests
+#python3 -m pip install requests
 
-cd $(dirname $0)/sources/
+#cd $(dirname $0)/sources/
 
 python3 AlQuranKareem.py > AlQuranKareem.m3u8
 
-echo m3u grabbed
+#echo m3u grabbed
