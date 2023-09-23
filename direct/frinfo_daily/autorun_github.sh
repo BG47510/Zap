df5cc0ad233399ee5b41b5e1b6676fcdf619ed4a
Zern <75311133+BG47510@users.noreply.github.com>
2023-09-23 17:06:56 +0200

    Update autorun_github.sh

--- direct/frinfo_daily/autorun_github.sh
+++ direct/frinfo_daily/autorun_github.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $(dirname $0)
-cd $(dirname $0)/scripts/
-python3 dailymotion_m3ugrabber.py > ../dailymotion-US.m3u
+cd $(dirname $0)/direct/frinfo_daily/
+python3 dailymotion_m3ugrabber.py > dailymotion-US.m3u
 
 echo m3u grabbed
