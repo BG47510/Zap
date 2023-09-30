57c768772fb4a7742467769ae2dfe9318d191369
Zern <75311133+BG47510@users.noreply.github.com>
2023-09-30 16:12:08 +0200

    Update motionsnif.py

--- motionsnif.py
+++ motionsnif.py
@@ -15,7 +15,7 @@ na = 'https://raw.githubusercontent.com/BG47510/Zap/main/assets/error.m3u8'
 def grab(line):
     try:
         _id = line.split('/')[4]
-        response = s.get(f'https://www.dailymotion.com/player/metadata/video/{_id}', proxies=proxies).json()['qualities']['auto'][0]['url']
+        response = s.get(f'https://www.dailymotion.com/player/metadata/video/{_id}').json()['qualities']['auto'][0]['url']
         m3u = s.get(response, proxies=proxies).text
         m3u = m3u.strip().split('\n')[1:]
         d = {}
