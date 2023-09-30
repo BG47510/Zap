932452221d528e01656858fdb14e7a7416889534
Zern <75311133+BG47510@users.noreply.github.com>
2023-09-30 16:36:57 +0200

    Update motionsnif.py

--- motionsnif.py
+++ motionsnif.py
@@ -15,8 +15,10 @@ na = 'https://raw.githubusercontent.com/BG47510/Zap/main/assets/error.m3u8'
 def grab(line):
     try:
         _id = line.split('/')[4]
-        response = s.get(f'https://www.dailymotion.com/player/metadata/video/{_id}').json()['qualities']['auto'][0]['url']
-        m3u = s.get(response).text
+        url = s.get(f'https://www.dailymotion.com/player/metadata/video/{_id}') # .json()['qualities']['auto'][0]['url']
+        response = requests.get(url).json()
+        flux = response["qualities"]["auto"][0]["url"]
+        m3u = s.get(flux).text
         m3u = m3u.strip().split('\n')[1:]
         #d = {}
         #cnd = True
