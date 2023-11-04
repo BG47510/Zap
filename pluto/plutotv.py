d3d0b88786c9bda943c64140875425f08ae496a5
Zern <75311133+BG47510@users.noreply.github.com>
2023-11-04 13:59:50 +0100

    Update plutotv.py

--- pluto/plutotv.py
+++ pluto/plutotv.py
@@ -12,12 +12,12 @@ api = requests.get(pluto).json()
 for p in api:
     print('')
     idflux = (f'{p["_id"]}') # aussi tvg-id
-    url = """"f'https://service-stitcher.clusters.pluto.tv/v1/stitch/embed/hls/channel/{idflux}" + \
-    "/master.m3u8?deviceId=channel&deviceModel=web&deviceVersion=1.0&appVersion=1.0" + \
-    "&deviceType=rokuChannel&deviceMake=rokuChannel&deviceDNT=1&advertisingId=channel" + \
-    "&embedPartner=rokuChannel&appName=rokuchannel&is_lat=1&bmodel=bm1&content=channel" + \
-    "&platform=web&tags=ROKU_CONTENT_TAGS&coppa=false&content_type=livefeed&rdid=channel" + \
-    "&genre=ROKU_ADS_CONTENT_GENRE&content_rating=ROKU_ADS_CONTENT_RATING" + \
+    url = """"f'https://service-stitcher.clusters.pluto.tv/v1/stitch/embed/hls/channel/{idflux}" +\
+    "/master.m3u8?deviceId=channel&deviceModel=web&deviceVersion=1.0&appVersion=1.0" +\
+    "&deviceType=rokuChannel&deviceMake=rokuChannel&deviceDNT=1&advertisingId=channel" +\
+    "&embedPartner=rokuChannel&appName=rokuchannel&is_lat=1&bmodel=bm1&content=channel" +\
+    "&platform=web&tags=ROKU_CONTENT_TAGS&coppa=false&content_type=livefeed&rdid=channel" +\
+    "&genre=ROKU_ADS_CONTENT_GENRE&content_rating=ROKU_ADS_CONTENT_RATING" +\
     "&studio_id=viacom&channel_id=channel'""""
     nom = (f',{p["name"]}')
     groupe = (f'#EXTINF:-1 group-title="{p["category"]}"')
