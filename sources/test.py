c3f70196a5284a00a2a39f5d4928d1f4cb98d537
Zern <75311133+BG47510@users.noreply.github.com>
2024-01-19 15:27:54 +0100

    Add files via upload

---
+++ sources/test.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python3
+# par github.com/BG47510
+
+import requests
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0",
+    "Referer": "https://www.lequipe.fr/tv/"
+}
+
+error = "https://raw.githubusercontent.com/BG47510/Zap/main/assets/error.m3u8"
+erreur = requests.get(error).text
+
+
+video = "https://www.dailymotion.com/player/metadata/video/x2lefik"
+code = requests.get(video, headers=headers).json()
+cle = "qualities"
+
+if cle in code:
+    flux = code["qualities"]["auto"][0]["url"]
+    m3u8 = requests.get(flux).text
+    print(m3u8)
+else:
+    print(erreur)
+
+
---
+++ sources/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 sources/test.py > sources/test.m3u8
+
+exit 0
