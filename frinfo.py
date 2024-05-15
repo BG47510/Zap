2008ac31a19b027700fee2f5ffd925839f0eb83f
Zern <75311133+BG47510@users.noreply.github.com>
2024-05-15 14:37:25 +0200

    Update frinfo.py

--- frinfo.py
+++ frinfo.py
@@ -18,49 +18,22 @@ headers = {
     "User-Agent": random.choice(ua),
 }
 
-
-https ='https://api.proxyscrape.com/?request=displayproxies&proxytype=https'
+https = 'https://api.proxyscrape.com/?request=displayproxies&proxytype=https'
 yui = requests.get(https).text.strip().split('\n')
-
-
-http ='https://api.proxyscrape.com/?request=displayproxies&proxytype=http'
+http = 'https://api.proxyscrape.com/?request=displayproxies&proxytype=http'
 aze = requests.get(http).text.strip().split('\n')
-
 # proxy > Country:China
-
 proxy = '117.146.231.40:9002'
-
-
-
-
 site = 'https://hdfauth.ftven.fr/esi/TA?url=https://simulcast-p.ftven.fr/simulcast/France_Info/hls_monde_frinfo/index.m3u8'
-
 response = requests.get(site, headers=headers, proxies={"http": proxy}, timeout=15).text
 
-
-
 lien = response.replace("index", "France_Info-avc1_2500000=10001")
-
 lien2 = response.replace("index", "France_Info-mp4a_96000_fra=20000")
 #print(f'#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio-AACL-96",LANGUAGE="fr",NAME="Francais",DEFAULT=YES,AUTOSELECT=YES,CHANNELS="2",URI="{lien2}"')
-
-
 # Les f-string permettent d’insérer des variables ou expressions à l'intérieur d'une chaine de caractères.
-
-
 flux = (f'\n#EXTM3U\n#EXT-X-VERSION:5\n#EXT-X-STREAM-INF:BANDWIDTH=3032655,AVERAGE-BANDWIDTH=2756959,CODECS="avc1.64001f,mp4a.40.2",RESOLUTION=1280x720,FRAME-RATE=25.000,AUDIO="audio-AACL-96",SUBTITLES="text"\n{lien}')
-
 flux2 = (f'\n#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio-AACL-96",LANGUAGE="fr",NAME="Francais",DEFAULT=YES,AUTOSELECT=YES,CHANNELS="2",URI="{lien2}"')
-
 m3u8 = (flux + flux2)
-
 print(m3u8)
 
 
-#if 'temp.txt' in os.listdir():
- #   os.system('rm temp.txt')
-  #  os.system('rm watch*')
-
-#fichier = open("frinfo.m3u8", "w")
-#fichier.write(m3u8)
-#fichier.close()
