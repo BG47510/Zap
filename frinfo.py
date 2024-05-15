5dfd3169f0cd613777fe15b14efdd356fae7d565
Zern <75311133+BG47510@users.noreply.github.com>
2024-05-15 14:39:33 +0200

    Update frinfo.py

--- frinfo.py
+++ frinfo.py
@@ -29,11 +29,9 @@ response = requests.get(site, headers=headers, proxies={"http": proxy}, timeout=
 
 lien = response.replace("index", "France_Info-avc1_2500000=10001")
 lien2 = response.replace("index", "France_Info-mp4a_96000_fra=20000")
-#print(f'#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio-AACL-96",LANGUAGE="fr",NAME="Francais",DEFAULT=YES,AUTOSELECT=YES,CHANNELS="2",URI="{lien2}"')
+# print(f'#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio-AACL-96",LANGUAGE="fr",NAME="Francais",DEFAULT=YES,AUTOSELECT=YES,CHANNELS="2",URI="{lien2}"')
 # Les f-string permettent d’insérer des variables ou expressions à l'intérieur d'une chaine de caractères.
 flux = (f'\n#EXTM3U\n#EXT-X-VERSION:5\n#EXT-X-STREAM-INF:BANDWIDTH=3032655,AVERAGE-BANDWIDTH=2756959,CODECS="avc1.64001f,mp4a.40.2",RESOLUTION=1280x720,FRAME-RATE=25.000,AUDIO="audio-AACL-96",SUBTITLES="text"\n{lien}')
 flux2 = (f'\n#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio-AACL-96",LANGUAGE="fr",NAME="Francais",DEFAULT=YES,AUTOSELECT=YES,CHANNELS="2",URI="{lien2}"')
 m3u8 = (flux + flux2)
 print(m3u8)
-
-
